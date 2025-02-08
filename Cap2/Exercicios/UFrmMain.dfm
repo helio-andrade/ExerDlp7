1bc05f73fc9221544a97edf41d1de489c1972ab4
Hélio Andrade <helio_andrade@hotmail.com.br>
2025-02-07 23:11:47 -0300

    Solução do Item 3

--- Cap2/Exercicios/UFrmMain.dfm
+++ Cap2/Exercicios/UFrmMain.dfm
@@ -2,7 +2,7 @@ object FrmMain: TFrmMain
   Left = 201
   Top = 127
   Width = 681
-  Height = 273
+  Height = 345
   Caption = 'Delphi 7 - Exerc'#237'cios'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -19,8 +19,8 @@ object FrmMain: TFrmMain
     Left = 0
     Top = 0
     Width = 665
-    Height = 234
-    ActivePage = TabSheet2
+    Height = 306
+    ActivePage = TabSheet3
     Align = alClient
     TabOrder = 0
     object TabSheet1: TTabSheet
@@ -66,6 +66,7 @@ object FrmMain: TFrmMain
         Font.Style = [fsItalic]
         ParentFont = False
         TabOrder = 0
+        OnKeyPress = edtCelsiusKeyPress
       end
       object Button1: TButton
         Left = 448
@@ -127,6 +128,7 @@ object FrmMain: TFrmMain
         Font.Style = [fsItalic]
         ParentFont = False
         TabOrder = 0
+        OnKeyPress = Edit1KeyPress
       end
       object Button2: TButton
         Left = 504
@@ -142,6 +144,97 @@ object FrmMain: TFrmMain
         ParentFont = False
         TabOrder = 1
         OnClick = Button2Click
+      end
+    end
+    object TabSheet3: TTabSheet
+      Caption = 'Volume'
+      ImageIndex = 2
+      object Label4: TLabel
+        Left = 64
+        Top = 32
+        Width = 176
+        Height = 27
+        Caption = 'Raio (em metros):'
+        Font.Charset = ANSI_CHARSET
+        Font.Color = clBlue
+        Font.Height = -21
+        Font.Name = 'Trebuchet MS'
+        Font.Style = [fsBold]
+        ParentFont = False
+      end
+      object Label5: TLabel
+        Left = 40
+        Top = 200
+        Width = 140
+        Height = 29
+        Caption = 'Resultado...'
+        Font.Charset = ANSI_CHARSET
+        Font.Color = clRed
+        Font.Height = -24
+        Font.Name = 'Trebuchet MS'
+        Font.Style = [fsBold, fsItalic]
+        ParentFont = False
+      end
+      object Label6: TLabel
+        Left = 48
+        Top = 80
+        Width = 192
+        Height = 27
+        Caption = 'Altura (em metros):'
+        Font.Charset = ANSI_CHARSET
+        Font.Color = clBlue
+        Font.Height = -21
+        Font.Name = 'Trebuchet MS'
+        Font.Style = [fsBold]
+        ParentFont = False
+      end
+      object Edit2: TEdit
+        Left = 262
+        Top = 25
+        Width = 193
+        Height = 40
+        BevelKind = bkFlat
+        BevelOuter = bvRaised
+        BorderStyle = bsNone
+        Font.Charset = ANSI_CHARSET
+        Font.Color = clBlack
+        Font.Height = -37
+        Font.Name = 'Consolas'
+        Font.Style = [fsItalic]
+        ParentFont = False
+        TabOrder = 0
+      end
+      object Edit3: TEdit
+        Left = 262
+        Top = 73
+        Width = 193
+        Height = 40
+        BevelKind = bkFlat
+        BevelOuter = bvRaised
+        BorderStyle = bsNone
+        Font.Charset = ANSI_CHARSET
+        Font.Color = clBlack
+        Font.Height = -37
+        Font.Name = 'Consolas'
+        Font.Style = [fsItalic]
+        ParentFont = False
+        TabOrder = 1
+        OnKeyPress = Edit3KeyPress
+      end
+      object Button3: TButton
+        Left = 376
+        Top = 128
+        Width = 83
+        Height = 33
+        Caption = 'Calcular'
+        Font.Charset = ANSI_CHARSET
+        Font.Color = clBlack
+        Font.Height = -16
+        Font.Name = 'Trebuchet MS'
+        Font.Style = [fsBold]
+        ParentFont = False
+        TabOrder = 2
+        OnClick = Button3Click
       end
     end
   end
--- Cap2/Exercicios/UFrmMain.pas
+++ Cap2/Exercicios/UFrmMain.pas
@@ -19,8 +19,20 @@ type
     Edit1: TEdit;
     Label3: TLabel;
     Button2: TButton;
+    TabSheet3: TTabSheet;
+    Label4: TLabel;
+    Edit2: TEdit;
+    Label5: TLabel;
+    Edit3: TEdit;
+    Label6: TLabel;
+    Button3: TButton;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure edtCelsiusKeyPress(Sender: TObject; var Key: Char);
+    procedure Edit1KeyPress(Sender: TObject; var Key: Char);
+    procedure Edit2KeyPress(Sender: TObject; var Key: Char);
+    procedure Edit3KeyPress(Sender: TObject; var Key: Char);
   private
     { Private declarations }
   public
@@ -41,14 +53,14 @@ begin
   // Verifica se o valor digitado em edtCelsius é um número válido
   if TryStrToFloat(edtCelsius.Text, Celsius) then
   begin
-    // Converte o valor de Celsius para Fahrenheit
+    // Converte o valor de Celsius para Fahrenheit e exibe no label
     lblResultadoFahrenheit.Caption := FormatFloat('0.00', Celsius) + ' ºC = ' +
-      FormatFloat('0.00', ConversaoFahrenheit(Celsius)) + ' F';
+      FormatFloat('0.00', ConversaoFahrenheit(Celsius)) + ' ºF';
   end
   else
   begin
     // Exibe uma mensagem de erro caso o valor não seja válido
-    ShowMessage('Por favor, insira um número válido!');
+    ShowMessage('Por favor, insira um número válido para Celsius!');
   end;
 end;
 
@@ -59,14 +71,67 @@ begin
   // Verifica se o valor digitado em Edit1 é um número válido
   if TryStrToFloat(Edit1.Text, Fahrenheit) then
   begin
-    // Converte o valor de Fahrenheit para Celsius
-    Label3.Caption := FormatFloat('0.00', Fahrenheit) + ' F = ' +
+    // Converte o valor de Fahrenheit para Celsius e exibe no label
+    Label3.Caption := FormatFloat('0.00', Fahrenheit) + ' ºF = ' +
       FormatFloat('0.00', ConversaoCelsius(Fahrenheit)) + ' ºC';
   end
   else
   begin
     // Exibe uma mensagem de erro caso o valor não seja válido
-    ShowMessage('Por favor, insira um valor válido!');
+    ShowMessage('Por favor, insira um número válido para Fahrenheit!');
+  end;
+end;
+
+procedure TFrmMain.Button3Click(Sender: TObject);
+var
+  Raio, Altura: Double;
+begin
+  // Verifica se os valores digitados são números válidos
+  if TryStrToFloat(Edit2.Text, Raio) and TryStrToFloat(Edit3.Text, Altura) then
+  begin
+    // Calcula o volume e exibe no label
+    Label5.Caption := 'Volume: ' + FormatFloat('0.0000', Volume(Raio, Altura)) + ' m³';
+  end
+  else
+  begin
+    // Exibe uma mensagem de erro caso algum valor não seja válido
+    ShowMessage('Por favor, insira valores válidos para Raio e Altura!');
+  end;
+end;
+
+procedure TFrmMain.edtCelsiusKeyPress(Sender: TObject; var Key: Char);
+begin
+  // Verifica se a tecla pressionada é Enter
+  if Key = #13 then
+  begin
+    Button1Click(Sender); // Chama o método de conversão de Celsius para Fahrenheit
+  end;
+end;
+
+procedure TFrmMain.Edit1KeyPress(Sender: TObject; var Key: Char);
+begin
+  // Verifica se a tecla pressionada é Enter
+  if Key = #13 then
+  begin
+    Button2Click(Sender); // Chama o método de conversão de Fahrenheit para Celsius
+  end;
+end;
+
+procedure TFrmMain.Edit2KeyPress(Sender: TObject; var Key: Char);
+begin
+  // Verifica se a tecla pressionada é Enter
+  if Key = #13 then
+  begin
+    Button3Click(Sender); // Chama o método de cálculo do volume
+  end;
+end;
+
+procedure TFrmMain.Edit3KeyPress(Sender: TObject; var Key: Char);
+begin
+  // Verifica se a tecla pressionada é Enter
+  if Key = #13 then
+  begin
+    Button3Click(Sender); // Chama o método de cálculo do volume
   end;
 end;
 
